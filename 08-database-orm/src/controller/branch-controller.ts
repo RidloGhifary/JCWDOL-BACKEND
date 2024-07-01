775061a3cfc68e759f760bca4b15661f171a788a
RidloGhifary <ridloghfry@gmail.com>
2024-07-01 21:42:48 +0700

    complete crud

--- 08-database-orm/src/controller/branch-controller.ts
+++ 08-database-orm/src/controller/branch-controller.ts
@@ -3,6 +3,23 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+export async function getBranch(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    const branch = await prisma.branch.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    res.status(200).json({ ok: true, branch });
+  } catch (err: any) {
+    console.error(err);
+    res
+      .status(err.statusCode || 500)
+      .json({ message: "Internal server error" });
+  }
+}
+
 export async function getBranches(req: Request, res: Response) {
   try {
     const branches = await prisma.branch.findMany();
@@ -27,6 +44,46 @@ export async function createBranch(req: Request, res: Response) {
     });
 
     res.status(201).json({ ok: true, message: "Branch created successfully" });
+  } catch (err: any) {
+    console.error(err);
+    res
+      .status(err.statusCode || 500)
+      .json({ message: "Internal server error" });
+  }
+}
+
+export async function updateBranch(req: Request, res: Response) {
+  const { id } = req.params;
+  const { name, location } = req.body;
+  try {
+    await prisma.branch.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        name: name,
+        location: location,
+        updatedAt: new Date(),
+      },
+    });
+    res.status(200).json({ ok: true, message: "Branch updated successfully" });
+  } catch (err: any) {
+    console.error(err);
+    res
+      .status(err.statusCode || 500)
+      .json({ message: "Internal server error" });
+  }
+}
+
+export async function deleteBranch(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    await prisma.branch.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    res.status(200).json({ ok: true, message: "Branch deleted successfully" });
   } catch (err: any) {
     console.error(err);
     res
--- 08-database-orm/src/router/branch-route.ts
+++ 08-database-orm/src/router/branch-route.ts
@@ -1,8 +1,19 @@
 import { Router } from "express";
-import { createBranch, getBranches } from "../controller/branch-controller";
+import {
+  createBranch,
+  getBranches,
+  getBranch,
+  updateBranch,
+  deleteBranch,
+} from "../controller/branch-controller";
 
 const router: Router = Router();
 
 router.route("/branch").get(getBranches).post(createBranch);
+router
+  .route("/branch/:id")
+  .get(getBranch)
+  .patch(updateBranch)
+  .delete(deleteBranch);
 
 export default router;
