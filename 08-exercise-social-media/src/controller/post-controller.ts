3f056f74cb045b82a73b0d895314be4c62bb732b
RidloGhifary <ridloghfry@gmail.com>
2024-07-02 13:04:48 +0700

    get posts and single post

--- 08-exercise-social-media/src/controller/post-controller.ts
+++ 08-exercise-social-media/src/controller/post-controller.ts
@@ -2,6 +2,39 @@ import { Response, Request, NextFunction } from "express";
 import { PostSchema } from "../schema";
 import db from "../lib/db";
 
+export async function getPosts(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const posts = await db.post.findMany();
+
+    res.status(200).json({ ok: true, data: posts });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function getSinglePost(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { id } = req.params;
+  try {
+    const post = await db.post.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    res.status(200).json({ ok: true, data: post });
+  } catch (err) {
+    next(err);
+  }
+}
+
 export async function createPost(
   req: Request,
   res: Response,
--- 08-exercise-social-media/src/router/post-router.ts
+++ 08-exercise-social-media/src/router/post-router.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { createPost } from "../controller/post-controller";
+import {
+  createPost,
+  getPosts,
+  getSinglePost,
+} from "../controller/post-controller";
 import { AuthenticatedToken } from "../middleware/auth-middleware";
 
 const router = Router();
 
 router.route("/posts").all(AuthenticatedToken).post(createPost);
+router.route("/posts").get(getPosts);
+router.route("/posts/:id").get(getSinglePost);
 
 export default router;
